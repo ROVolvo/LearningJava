72711b6ab214dc46a92dd2dabf3eaa15555c0fb9
Johnn <Johnn@Gammanite>
2020-07-28 17:14:58 +0100

    First commit - im learning java

---
+++ LearningJava/src/me/rovolvo/learningjava/Main.java
@@ -0,0 +1,13 @@
+package me.rovolvo.learningjava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(imLearningJava());
+	}
+	
+	public static String imLearningJava() {
+		return "Im Learning Java Today!";
+	}
+	
+}
